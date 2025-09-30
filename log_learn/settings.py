96c9fca2d348304177e1cc461b5268518b156023
shrimandharjivandharmagdum <shrimandharjivandharmagdum@gmail.com>
2025-09-30 11:50:15 +0530

    cred hide

--- log_learn/settings.py
+++ log_learn/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shrimandharjivandharmagdum@gmail.com'
-EMAIL_HOST_PASSWORD = 'goknkqyyspeyuljq'
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
